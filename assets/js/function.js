0c053c7f1020d0e12387002f23e4db9cf37bf0f4
ahmad fauzan <fauzan.c4@gmail.com>
2022-01-08 12:18:39 +0700

    change color

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,8 +1,8 @@
 let cardExperience = (image, company, position, period) => {
     let dataImage;
-    dataImage = image !== "" ? `<img src="${image}" style="width: 100px">` : '<span class="material-icons deep-orange-text text-darken-2" style="width: 100px">work</span>';
+    dataImage = image !== "" ? `<img src="${image}" style="width: 100px">` : '<span class="material-icons text-primary" style="width: 100px">work</span>';
     return `<div class="col s12 m12">
-                <div class="card horizontal z-depth-3 grey lighten-4">
+                <div class="card horizontal z-depth-3 bg-light">
                     <div class="card-stacked">
                         <div class="card-content">
                             ${dataImage}
@@ -18,12 +18,12 @@ let cardEducation = (image, education, degree, major, period, thesis) => {
     let dataThesis = "";
     if (thesis !== "") {
         dataThesis = `<blockquote>
-                        Judul Skripsi: <a href="${thesis.url}" target="_blank" rel="noopener noreferrer">${thesis.name}</a>
+                        Judul Skripsi: <a href="${thesis.url}" target="_blank" class="text-red-light" rel="noopener noreferrer">${thesis.name}</a>
                      </blockquote>`
     }
 
     return `<div class="col s12 m12">
-                <div class="card horizontal z-depth-3 orange lighten-5">
+                <div class="card horizontal z-depth-3 bg-light">
                     <div class="card-stacked">
                         <div class="card-content">
                             <img src="${image}" style="width: 100px">
@@ -44,10 +44,10 @@ let cardPortfolio = (image, title, description, url) => {
                         <img class="activator" src="${image}" alt="${title}" style="height: 200px"/>
                     </div>
                     <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">${title}<i class="material-icons right">more_vert</i></span>
+                        <span class="card-title activator text-primary">${title}<i class="material-icons right">more_vert</i></span>
                     </div>
                     <div class="card-action">
-                        <a href="${url}" class="waves-effect waves-light btn-small deep-orange darken-2"><i class="material-icons right">send</i>Detail</a>
+                        <a href="${url}" class="waves-effect waves-light btn-small bg-primary"><i class="material-icons right">send</i>Detail</a>
                     </div>
                     <div class="card-reveal">
                         <span class="card-title grey-text text-darken-4">${title}<i class="material-icons right">close</i></span>
@@ -59,7 +59,7 @@ let cardPortfolio = (image, title, description, url) => {
 
 let cardAward = (image, publisher, certification, description, period, url) => {
     return `<div class="col s12 m12">
-                <div class="card horizontal z-depth-3 grey lighten-4">
+                <div class="card horizontal z-depth-3 bg-light">
                     <div class="card-stacked">
                         <div class="card-content">
                             <img src="${image}" alt="${publisher}" style="width: 100px">
@@ -70,7 +70,7 @@ let cardAward = (image, publisher, certification, description, period, url) => {
                             </blockquote>
                         </div>
                         <div class="card-action">
-                            <a href="${url}" class="waves-effect waves-light btn-small right deep-orange darken-2" target="_blank" rel="noopener noreferrer"><i class="material-icons right">send</i>Sertifikat</a>
+                            <a href="${url}" class="waves-effect waves-light btn-small right bg-primary" target="_blank" rel="noopener noreferrer"><i class="material-icons right">send</i>Sertifikat</a>
                         </div>
                     </div>
                 </div>
