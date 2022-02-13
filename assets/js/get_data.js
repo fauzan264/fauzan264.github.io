0545e50cb92464930f0dae13642453205f1670cc
ahmad fauzan <fauzan.c4@gmail.com>
2022-02-13 16:57:04 +0700

    update get_data

--- assets/js/get_data.js
+++ assets/js/get_data.js
@@ -15,22 +15,22 @@ xhttp.send();
 
 experienceData();
 
-let educationsData = () => {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let data = JSON.parse(xhttp.responseText);
-            data.forEach(e => {
-              document.getElementById("education").innerHTML += cardEducation(e.image, e.education, e.degree, e.major, e.period, e.thesis);
-            });
-        }
-    };
+// let educationsData = () => {
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             let data = JSON.parse(xhttp.responseText);
+//             data.forEach(e => {
+//               document.getElementById("education").innerHTML += cardEducation(e.image, e.education, e.degree, e.major, e.period, e.thesis);
+//             });
+//         }
+//     };
 
-xhttp.open("GET", "assets/data/educations.json", true);
-xhttp.send();
-}
+// xhttp.open("GET", "assets/data/educations.json", true);
+// xhttp.send();
+// }
 
-educationsData();
+// educationsData();
 
 let portfoliosData = () => {
     let xhttp = new XMLHttpRequest();
